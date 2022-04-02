a7e5336bebf47c16e9204982cd810fd95f1b8875
Scott Steffes <swsteffes@gmail.com>
2022-04-02 11:43:29 -0500

    progress

---
+++ Assets/Scripts/FileListResponse.cs
@@ -0,0 +1,23 @@
+using System;
+using Newtonsoft.Json;
+
+[Serializable]
+public class FileListResponse {
+    public FileData[] step1;
+    public FileData[] step2;
+
+    public static FileListResponse CreateFromJsonString(string json) {
+        return JsonConvert.DeserializeObject<FileListResponse>(json);
+    }
+}
+
+[Serializable]
+public class FileData {
+    public string id;
+    public int iteration;
+    public int date;
+    public int likes;
+    public int dislikes;
+    public string path;
+    public string originalPath;
+}
