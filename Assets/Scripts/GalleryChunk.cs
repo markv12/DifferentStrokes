d05f4cfcda67909590580a25524c7c6dfc01641a
Scott Steffes <swsteffes@gmail.com>
2022-04-03 12:54:22 -0500

    move chunks

--- Assets/Scripts/GalleryChunk.cs
+++ Assets/Scripts/GalleryChunk.cs
@@ -1,7 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GalleryChunk : MonoBehaviour {
+    public Transform t;
     public Transform[] paintingSpawnLocations;
+
+    public void MoveToPosition(Vector3 endPos) {
+        Vector3 startPos = endPos.SetY(endPos.y - 10f);
+        t.position = startPos;
+        this.CreateAnimationRoutine(1.5f, (float progress) => {
+            float easedProgress = Easing.easeOutCubic(0f, 1f, progress);
+            t.position = Vector3.Lerp(startPos, endPos, easedProgress);
+        });
+    }
 }
